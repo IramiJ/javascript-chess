9c194cc233af011e25861b74738904364da9dc78
Colliname <yugiohduel3@gmail.com>
2022-10-31 19:57:22 +0100

    added images and made a board

---
+++ index.js
@@ -0,0 +1,13 @@
+var turn = 'w'
+var chessboard = [['T_black','S_black', 'L_black', 'D_black', 'K_black', 'L_black', 'S_black', 'T_black'],
+              ['B_black','B_black','B_black','B_black','B_black','B_black','B_black','B_black'],
+              ['0','0','0','0','0','0','0','0'],
+              ['0','0','0','0','0','0','0','0'],
+              ['0','0','0','0','0','0','0','0'],
+              ['0','0','0','0','0','0','0','0'],
+              ['B_white','B_white','B_white','B_white','B_white','B_white','B_white','B_white'],
+              ['T_white','S_white', 'L_white', 'D_white', 'K_white', 'L_white', 'S_white', 'T_white']]
+
+for (let i = 0; i<=7; i++){
+    
+}
