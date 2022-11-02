1703160ed7a3084940d10f9080fdb5fd7ab47b62
Colliname <yugiohduel3@gmail.com>
2022-11-02 20:05:20 +0100

    made the move system work

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 var turn = 'w'
-var chessboard = [['T_black','S_black','L_black','D_black', 'K_black', 'L_black', 'S_black', 'T_black'],
-              ['B_black','B_black','B_black','B_black','B_black','B_black','B_black','B_black',],
+var chessboard = [['T_black,0','S_black,0','L_black,0','D_black,0', 'K_black,0', 'L_black,1', 'S_black,1', 'T_black,1'],
+              ['B_black,0','B_black,1','B_black,2','B_black,3','B_black,4','B_black,5','B_black,6','B_black,7'],
               ['0','0','0','0','0','0','0','0'],
               ['0','0','0','0','0','0','0','0'],
               ['0','0','0','0','0','0','0','0'],
               ['0','0','0','0','0','0','0','0'],
-              ['B_white','B_white','B_white','B_white','B_white','B_white','B_white','B_white'],
-              ['T_white','S_white','L_white','D_white', 'K_white', 'L_white', 'S_white', 'T_white']]
+              ['B_white,0','B_white,1','B_white,2','B_white,3','B_white,4','B_white,5','B_white,6','B_white,7'],
+              ['T_white,0','S_white,0','L_white,0','D_white,0', 'K_white,0', 'L_white,1', 'S_white,1', 'T_white,1']]
 
     
 loadChessboard()
 dragPiece()
-function initialize(){
-
-}
+makeMove()
+//loads the chessboard
 function loadChessboard(){
     for(i=7; i>=0; i--){
         for (j=0; j<=7; j+=1){
@@ -23,15 +22,16 @@ function loadChessboard(){
                 row = document.getElementById(id.toString());
                 squares = row.childNodes;
                 var img = document.createElement("img")
-                img.setAttribute("src", "imgs/" + piece + ".png");
+                img.setAttribute("src", "imgs/" + piece.split(",")[0] + ".png");
                 img.setAttribute('class', 'piece')
-                img.setAttribute("id", piece)
+                img.setAttribute("id", piece.split(",")[0]+piece.split(",")[1])
                 squares[(j+1)*2-1].appendChild(img);
             }
         }
     }
 }
 
+//handeling dragging and droping of the pieces
 function dragPiece(){
     const squares = document.querySelectorAll('.black, .white')
     const pieces = document.querySelectorAll('.piece')
@@ -41,17 +41,35 @@ function dragPiece(){
     squares.forEach(square => {
         square.addEventListener('dragenter', dragEnter)
         square.addEventListener('dragover', dragOver);
-        square.addEventListener('dragleave', dragLeave);
         square.addEventListener('drop', drop);
     });
 }
 function dragStart(e){
-    e.dataTransfer.setData('text/plain', e.target.id);
-    setTimeout(() => {
-        e.target.classList.add('hide');
-    }, 0);
+    if (turn == "w"){
+        if(e.target.id.split("_")[1].includes("white")){
+            e.dataTransfer.setData('text/plain', e.target.id);
+            setTimeout(() => {
+                e.target.classList.add('hide');
+            }, 0);
+        }
+        else{
+            return null;
+        }
+        turn = "b"
+    }
+    else{
+        if(e.target.id.split("_")[1].includes("black")){
+            e.dataTransfer.setData('text/plain', e.target.id);
+            setTimeout(() => {
+                e.target.classList.add('hide');
+            }, 0);
+        }
+        else{
+            return null;
+        }
+        turn = "w"
 }
-
+}
 function dragEnter(e) {
     e.preventDefault();
 }
@@ -60,8 +78,6 @@ function dragOver(e) {
     e.preventDefault();
 }
 
-function dragLeave(e) {
-}
 
 function drop(e) {
 
@@ -73,5 +89,5 @@ function drop(e) {
     e.target.appendChild(draggable);
 
     // display the draggable element
-    draggable.classList.remove('hide');
+  draggable.classList.remove('hide');
 }
